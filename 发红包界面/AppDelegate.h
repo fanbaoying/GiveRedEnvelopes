68f8c8ce59c2d76a93695cf33b5ebdfc091e36b9
董君龙 <763560768@qq.com>
2018-02-07 14:05:40 +0800

    Initial Commit

---
+++ 发红包界面/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  发红包界面
+//
+//  Created by cheBaidu on 2018/2/7.
+//  Copyright © 2018年 车佰度. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 发红包界面/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  发红包界面
+//
+//  Created by cheBaidu on 2018/2/7.
+//  Copyright © 2018年 车佰度. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
